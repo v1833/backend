54bf8d5f4c887cb331f106aec1d8aa37dbc90456
crynchyrollsushiisamyth <viacheslav.riashchentcev@gmail.com>
2023-06-29 10:13:26 -0600

    Revert "deleted todo"
    This reverts commit fe77a116f0834228b72c1a9c53070af84e3c2cec.

--- src/main/java/org/example/UserController.java
+++ src/main/java/org/example/UserController.java
@@ -16,6 +16,7 @@ public class UserController {
         }
     }
 
+    //todo 1) return proper status code 2) return id after post
     @DeleteMapping("/users/{id}")
     public void removeUser(@PathVariable long id) {
         for (User user : users) {
